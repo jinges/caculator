451804e56f27e77724341e7eb4d0001b23ad7c68
jinges <jinges2012@gmail.com>
2019-10-22 17:33:39 +0800

    add page

---
+++ lib/screen.dart
@@ -0,0 +1,19 @@
+/*
+ * @Author: 大明冯 
+ * @Date: 2019-10-22 17:31:43 
+ * @Last Modified by: 大明冯
+ * @Last Modified time: 2019-10-22 17:33:21
+ */
+
+import 'package:flutter/material.dart';
+
+class ScreenWidget extends StatelessWidget {
+  const ScreenWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ,
+    );
+  }
+}
